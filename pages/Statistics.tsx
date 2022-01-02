174eff31e7abece35858d13eb07cc8e4df07a3ed
Burt <2902978956@qq.com>
2022-01-02 18:49:56 +0800

    创建记账页、统计页、我的信息页

---
+++ pages/Statistics.tsx
@@ -0,0 +1,15 @@
+/**
+ * @description 统计页面
+ * */
+
+import {NextPage} from 'next';
+
+const Statistics: NextPage = () => {
+  return (
+    <div>
+      统计页
+    </div>
+  )
+}
+
+export default Statistics
---
+++ pages/personal.tsx
@@ -0,0 +1,15 @@
+/**
+ * @description 个人信息页面
+ * */
+
+import {NextPage} from 'next';
+
+const Personal: NextPage = () => {
+  return (
+    <div>
+      个人信息页面
+    </div>
+  );
+};
+
+export default Personal;
