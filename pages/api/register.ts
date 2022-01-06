8cf1f2a2e81a28276565dbcdf65cee96197dabec
黄冰 <2902978956@qq.com>
2022-01-06 15:59:09 +0800

    密码加密，username唯一性校验

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,5 +1,6 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {PrismaClient} from '@prisma/client';
+import sha256 from 'crypto-js/sha256';
 
 const prisma = new PrismaClient();
 
@@ -22,9 +23,12 @@ export default async function handler(
   if (method !== 'POST') return res.status(404).json({message: 'Not Found'});
 
   const {username, password} = body as RequestData;
+  const user = await prisma.user.findUnique({where: {username: username}});
+  if (user) return res.status(422).json({message: `${username} already exists`});
 
   const result = await prisma.user.create({
-    data: {username, password}
+    data: {username, password: sha256(password).toString()},
+    select: {password: false, id: true, username: true, createdAt: true, avatar: true}
   });
   await prisma.$disconnect();
   res.json(result);
