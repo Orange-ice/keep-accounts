79e63dba5d1c4eafc2cca65855f42c739ed4b31a
burt <2902978956@qq.com>
2022-02-07 15:09:36 +0800

    账单添加日期分类

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,6 +35,8 @@ const Home: NextPage = () => {
   const router = useRouter();
 
   const [records, setRecords] = React.useState<Record[]>([]);
+  const [consumption, setConsumption] = React.useState<{ [K: string]: string }>({});
+
   const addRecords = () => {
     router.push('/records');
   };
@@ -45,7 +47,28 @@ const Home: NextPage = () => {
   const getRecords = () => {
     axios.get('/api/v1/record').then(res => {
       setRecords(res.data);
+      computeOfDay(res.data);
+    }).catch(() => {
+      router.push('/login');
     });
+  };
+
+  /**
+   * 计算每天的总消费
+   * */
+  const computeOfDay = (records: Record[]) => {
+    const dateMap: { [K: string]: number } = {};
+    records.forEach(r => {
+      const date = dayjs(r.createdAt).format('YYYY-MM-DD');
+      dateMap[date] = (dateMap[date] ?? 0) + parseFloat(`${r.tag.type ? '' : '-'}${r.amount}`);
+    });
+
+    const result: { [K: string]: string } = {};
+    Object.keys(dateMap).forEach(item => {
+      result[item] = dateMap[item] > 0 ? `收入${Math.abs(dateMap[item])}` : `支出${Math.abs(dateMap[item])}`;
+    });
+
+    setConsumption(result);
   };
 
   const formatDate = (date: string) => {
@@ -53,13 +76,19 @@ const Home: NextPage = () => {
     if (dayjs(date).isToday()) week = '今天';
     if (dayjs(date).isYesterday()) week = '昨天';
 
-    let formattedDate = '';
+    let formattedDate: string;
     if (dayjs().isSame(date, 'year')) {
       formattedDate = `${dayjs(date).format('MM月DD日')} ${week}`;
     } else {
       formattedDate = `${dayjs(date).format('YYYY/MM/DD')} ${week}`;
     }
     return formattedDate;
+  };
+
+  // 同一天只显示一次日期，判断是否显示日期
+  const judgeHeader = (date: string, index: number) => {
+    if (index === 0) return true;
+    return !dayjs(date).isSame(dayjs(records[index - 1].createdAt), 'day');
   };
 
   React.useEffect(() => {
@@ -89,19 +118,22 @@ const Home: NextPage = () => {
         </div>
 
         {/* 账单 */}
-        {records.map((item, index) => <div key={item.id} className={styles.records}>
-          <header>
-            <span>{formatDate(item.createdAt)}</span>
-            {/*<span>{item.tag.type ? '收入' : '支出'}{item.amount}</span>*/}
-          </header>
-          <div className={styles.details}>
-            <div className={styles.iconBox} style={{backgroundColor: colors[item.tag.icon]}}>
-              <Icon name={item.tag.icon}/>
+        <div className={styles.recordsWrapper}>
+          {records.map((item, index) => <div key={item.id} className={styles.records}>
+            {judgeHeader(item.createdAt, index) &&
+              <header>
+                <span>{formatDate(item.createdAt)}</span>
+                <span>{consumption[dayjs(item.createdAt).format('YYYY-MM-DD')]}</span>
+              </header>}
+            <div className={styles.details}>
+              <div className={styles.iconBox} style={{backgroundColor: colors[item.tag.icon]}}>
+                <Icon name={item.tag.icon}/>
+              </div>
+              <span className={styles.tagName}>{item.tag.name}</span>
+              <span>{item.tag.type ? '+' : '-'}{item.amount}</span>
             </div>
-            <span className={styles.tagName}>{item.tag.name}</span>
-            <span>{item.tag.type ? '+' : '-'}{item.amount}</span>
-          </div>
-        </div>)}
+          </div>)}
+        </div>
 
         <div className={styles.add} onClick={addRecords}>
           <Icon name="tianjia"/>
